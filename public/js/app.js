e7e414f4358a0adbf69b184e1b7ef39cd906bc42
Adamelbouchiri <matrakmtk@gmail.com>
2024-08-15 22:47:53 +0200

    add-modal-and-testimonials-testing

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,29 @@ const menuDivs = document.querySelectorAll(".ourMenu-body");
 
 menuTitles.forEach((title) => {
   title.addEventListener("click", () => {
-    menuDivs.forEach(div => {
-      div.classList.remove("active")
-      if(title.id === div.id) {
-        div.classList.add("active")
+    menuDivs.forEach((div) => {
+      div.classList.remove("active");
+      if (title.id === div.id) {
+        div.classList.add("active");
       }
-    })
+    });
 
+    menuTitles.forEach((ele) => {
+      ele.classList.remove("active");
+    });
 
-    menuTitles.forEach(ele => {
-      ele.classList.remove("active")
-    })
-
-    title.classList.add("active")
+    title.classList.add("active");
   });
+});
+
+let modal = document.querySelector(".my-modal");
+let modalShowBtn = document.querySelector(".modal-button");
+let modalCloseBtn = document.querySelector(".my-modal .close");
+
+modalShowBtn.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
 });

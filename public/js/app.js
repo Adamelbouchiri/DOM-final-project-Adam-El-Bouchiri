6f5bf8c3451ba0c44473f225c6321efcc5a109bd
Adamelbouchiri <matrakmtk@gmail.com>
2024-08-15 23:42:57 +0200

    add-second-vid-and-fixing-videos-play-and-stop

--- public/js/app.js
+++ public/js/app.js
@@ -31,14 +31,34 @@ modalCloseBtn.addEventListener("click", () => {
 });
 
 let firstVid = document.querySelector(".first-video-div");
+let firstVidVideo = document.querySelector(".first-video-div video");
 let firstVidClose = document.querySelector(".first-video-div span");
 let firstVidOpen = document.querySelector(".watch-vid")
 
 firstVidOpen.addEventListener("click", () => {
   firstVid.classList.remove("hide")
+  firstVidVideo.play();
 })
 
 firstVidClose.addEventListener("click", () => {
   firstVid.classList.add("hide")
+  firstVidVideo.pause();
+  firstVidVideo.currentTime = 0;
+})
+
+let scndVid = document.querySelector(".scnd-vid-div");
+let scndVidVideo = document.querySelector(".scnd-vid-div video");
+let scndVidClose = document.querySelector(".scnd-vid-div span");
+let scndVidOpen = document.querySelector(".scnd-vid-watch")
+
+scndVidOpen.addEventListener("click", () => {
+  scndVid.classList.remove("hide")
+  scndVidVideo.play()
+})
+
+scndVidClose.addEventListener("click", () => {
+  scndVid.classList.add("hide")
+  scndVidVideo.pause();
+  scndVidVideo.currentTime = 0;
 })
 
